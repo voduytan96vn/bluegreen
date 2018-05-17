ab5a5a7f4f86f48850899578469dc6667d96d5da
Vo Duy Tan <voduytan96.vn@gmail.com>
2018-05-17 17:59:53 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,16 @@
 <?php
 // Create a 55x30 image
-$im = imagecreatetruecolor(200, 200);
-$white = imagecolorallocate($im, 255, 255, 255);
-$blue = imagecolorallocate($im, 0, 0, 255);
-$green = imagecolorallocate($im, 0, 255, 0);
+//$im = imagecreatetruecolor(200, 200);
+//$white = imagecolorallocate($im, 255, 255, 255);
+//$blue = imagecolorallocate($im, 0, 0, 255);
+//$green = imagecolorallocate($im, 0, 255, 0);
 // Draw a filled rectangle
 //imagefilledrectangle($im, 0, 0, 199, 199, $blue);
-imagefilledrectangle($im, 0, 0, 199, 199, $green);
+//imagefilledrectangle($im, 0, 0, 199, 199, $green);
 // Save the image
-header('Content-Type: image/png');
-imagePNG($im);
-imagedestroy($im);
+//header('Content-Type: image/png');
+//imagePNG($im);
+//imagedestroy($im);
+echo "I am VERSION 1 <br><br>";
+echo "My Pod IP is : ".$_SERVER['SERVER_ADDR'];
 ?>
